d373cd442de79c786535f84180c90c4569668db7
E.Koretsky <E.Koretsky@outlook.com>
2022-08-21 22:54:30 +0300

    refactor for max length tests

--- test/max_length_test.dart
+++ test/max_length_test.dart
@@ -3,48 +3,46 @@ import 'package:validation_assistant/validation_assistant.dart';
 
 void main() {
   group('Test group for maxLength', () {
-    test('value length less than maxLength', () {
+    test('value length less than maxLength without users message', () {
       final validationAssistant = ValidationAssistant();
 
       validationAssistant.maxLength(5);
       expect(validationAssistant('test'), isNull);
     });
 
-    test('value length less than maxLength with custom message', () {
+    test('value length less than maxLength with users message', () {
       final validationAssistant = ValidationAssistant();
 
-      validationAssistant.maxLength(5, 'custom error message');
+      validationAssistant.maxLength(5, 'users error message');
       expect(validationAssistant('test'), isNull);
     });
 
-    test('value length equal maxLength', () {
+    test('value length equal maxLength without users message', () {
       final validationAssistant = ValidationAssistant();
 
       validationAssistant.maxLength(5);
       expect(validationAssistant('test5'), isNull);
     });
 
-    test('value length equal maxLength with custom message', () {
+    test('value length equal maxLength with users message', () {
       final validationAssistant = ValidationAssistant();
 
-      validationAssistant.maxLength(5, 'custom error message');
+      validationAssistant.maxLength(5, 'users error message');
       expect(validationAssistant('test5'), isNull);
     });
 
-    test('value length more than maxLength', () {
+    test('value length more than maxLength without users message', () {
       final validationAssistant = ValidationAssistant();
 
       validationAssistant.maxLength(5);
-      expect(
-          validationAssistant.call('length more than 5'), 'length more than 5');
+      expect(validationAssistant('length more than 5'), 'length more than 5');
     });
 
-    test('value length more than maxLength with custom error message', () {
+    test('value length more than maxLength with users error message', () {
       final validationAssistant = ValidationAssistant();
 
-      validationAssistant.maxLength(5, 'custom error message');
-      expect(
-          validationAssistant('value more than five'), 'custom error message');
+      validationAssistant.maxLength(5, 'users error message');
+      expect(validationAssistant('value more than five'), 'users error message');
     });
   });
 }
