9d6185ca163cda954683b749ab9b5aee49655e09
E.Koretsky <E.Koretsky@outlook.com>
2022-08-21 22:53:03 +0300

    refactor for required test

--- test/required_test.dart
+++ test/required_test.dart
@@ -3,24 +3,45 @@ import 'package:validation_assistant/validation_assistant.dart';
 
 void main() {
   group('Test group for required ', () {
-    test('value is null', () {
+    test('value is null without users message', () {
       final validationAssistant = ValidationAssistant();
 
       validationAssistant.required();
       expect(validationAssistant(null), 'required');
     });
 
-    test('value equal empty string', () {
+    test('value is null with users message', () {
+      final validationAssistant = ValidationAssistant();
+
+      validationAssistant.required('users error message');
+      expect(validationAssistant(null), 'users error message');
+    });
+
+    test('value equal empty string without users message', () {
       final validationAssistant = ValidationAssistant();
 
       validationAssistant.required();
       expect(validationAssistant(''), 'required');
     });
 
-    test('value is not null and is not empty string', () {
+    test('value equal empty string with users message', () {
+      final validationAssistant = ValidationAssistant();
+
+      validationAssistant.required('users error message');
+      expect(validationAssistant(''), 'users error message');
+    });
+
+    test('value is not null and is not empty string without users message', () {
       final validationAssistant = ValidationAssistant();
 
       validationAssistant.required();
+      expect(validationAssistant('value'), isNull);
+    });
+
+    test('value is not null and is not empty string with users message', () {
+      final validationAssistant = ValidationAssistant();
+
+      validationAssistant.required('users error message');
       expect(validationAssistant('value'), isNull);
     });
   });
